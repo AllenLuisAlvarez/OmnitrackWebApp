a751ed4b22e898a36ba098a196fbd777e472f5ea
Allen Luis Alvarez <allen.luis626@gmail.com>
2024-12-15 03:10:38 +0800

    test

--- frontend/src/Pages/LoginSignUpPage/Login.jsx
+++ frontend/src/Pages/LoginSignUpPage/Login.jsx
@@ -23,7 +23,7 @@ const Login = () => {
     }
 
     try {
-      const response = await fetch("http://localhost:8800/login", {
+      const response = await fetch("https://omnitrackwebapp.onrender.com/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
